9976561bd38f9ff065ba5e1a1e37ee3836307a2b
Cristhian <cristhiantilleria@gmail.com>
2022-04-10 19:08:47 -0400

    initial-commit

---
+++ template-parts/content.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nomad-sun
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+
+	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<!-- <?php nomad_sun_entry_footer(); ?> -->
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
